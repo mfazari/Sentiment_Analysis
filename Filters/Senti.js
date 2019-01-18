7ed8eaddb089f23d803da5ad33b4c3f30b828115
mfazari <massimofazari@bluewin.ch>
2019-01-18 12:33:40 +0100

    Working

--- Filters/Senti.js
+++ Filters/Senti.js
@@ -5,28 +5,21 @@ let sentiment = require('sentiment');
 //function retrieves sentiment-score
 module.exports = function analyze(sentences) {
     var result_object = 0;
-    var score = 0;
+    var result_score = 0;
+    var length = sentences.length;
+
+    //Put all the sentences in one massive String
+    var joint = sentences.join();
 
     //instantiate our sentiment function
     var sent = new sentiment();
-    var length = sentences.length;
-    for(var i = 0; i < length; i++) {
-        result_object = sent.analyze(sentences[i]);
-    }
-    score = result_object.score / length;
-    return score;
+
+    //... and use it with out String full of Tweets
+    result_object = sent.analyze(joint);
+
+    //Filter out the score
+    result_score = result_object.score / length;
+
+    //... and return the result
+    return result_score;
 };
-
-
-
-/*
-module.exports = function analyze(sentence) {
-
-
-
-
-
- return result;
-};
-
-*/
